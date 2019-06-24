03ab16c2afb082c86031f5750df731091cab3ceb
Zurglots <bfeole@gmail.com>
2019-06-24 18:14:52 -0400

    Add redux file structure add reducer boilerplate

--- clinicaltrialfinder/src/index.js
+++ clinicaltrialfinder/src/index.js
@@ -2,5 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore, compose } from "redux";
+import rootReducer from "./reducers";
+// import logger from "redux-logger";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
