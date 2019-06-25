4a2fd88b7fccc4dc8a87fc68bc1562bbf72e6e38
Zurglots <bfeole@gmail.com>
2019-06-24 22:40:50 -0400

    Actions reducers axioswithAuth added boilerplate

--- clinicaltrialfinder/src/actions/index.js
+++ clinicaltrialfinder/src/actions/index.js
@@ -1,11 +1,27 @@
 import axios from "axios";
 
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+export const login = creds => dispatch => {
+  dispatch({ type: LOGIN_START });
+  return axiosWithAuth()
+    .post("/login", creds)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload);
+      dispatch({ type: LOGIN_SUCCESS });
+      return true;
+    })
+    .catch(err => console.log(err.response));
+};
+
 export const FETCH_DATA_START = "FETCH_DATA_START";
 export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
 export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE";
 
-export const getTrials = trial => dispatch => {
-  console.log("get trial running", trial); // figure out what data looks like
+export const getData = data => dispatch => {
+  console.log("getData start", data); // don't know what data looks like
   dispatch({ type: FETCH_DATA_START });
   axios
     .get("HEROKU")
--- clinicaltrialfinder/src/reducers/index.js
+++ clinicaltrialfinder/src/reducers/index.js
@@ -1,7 +1,48 @@
-const initialState = {};
+import {
+  LOGIN_START,
+  LOGIN_SUCCESS,
+  FETCH_DATA_START,
+  FETCH_DATA_SUCCESS,
+  FETCH_DATA_FAILURE
+} from "../actions";
+
+const initialState = {
+  error: "",
+  loggingIn: false
+};
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        error: "",
+        loggingIn: true
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        loggingIn: false,
+        error: ""
+      };
+    case FETCH_DATA_START:
+      return {
+        ...state,
+        error: "",
+        fetchingData: true
+      };
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        fetchingData: false
+      };
+    case FETCH_DATA_FAILURE:
+      return {
+        ...state,
+        fetchingData: false,
+        error: action.payload
+      };
+
     default:
       return state;
   }
