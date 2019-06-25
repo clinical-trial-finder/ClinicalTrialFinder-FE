8d21cd681dcc56c83e45e4f0a3f80c2e0db5e1ea
Zurglots <bfeole@gmail.com>
2019-06-24 22:42:39 -0400

    Added comments

--- clinicaltrialfinder/src/actions/index.js
+++ clinicaltrialfinder/src/actions/index.js
@@ -4,17 +4,17 @@ export const LOGIN_START = "LOGIN_START";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAILURE = "LOGIN_FAILURE";
 
-export const login = creds => dispatch => {
-  dispatch({ type: LOGIN_START });
-  return axiosWithAuth()
-    .post("/login", creds)
-    .then(res => {
-      localStorage.setItem("token", res.data.payload);
-      dispatch({ type: LOGIN_SUCCESS });
-      return true;
-    })
-    .catch(err => console.log(err.response));
-};
+// export const login = creds => dispatch => {
+//   dispatch({ type: LOGIN_START });
+//   return axiosWithAuth()
+//     .post("/login", creds)
+//     .then(res => {
+//       localStorage.setItem("token", res.data.payload);
+//       dispatch({ type: LOGIN_SUCCESS });
+//       return true;
+//     })
+//     .catch(err => console.log(err.response));
+// };
 
 export const FETCH_DATA_START = "FETCH_DATA_START";
 export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
