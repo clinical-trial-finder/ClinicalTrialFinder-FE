39140cc897c838fe793ccd49d996ba21a96873d2
Zurglots <bfeole@gmail.com>
2019-06-24 22:55:04 -0400

    Trials base components added

---
+++ clinicaltrialfinder/src/components/TrialsList.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+// add withrouter from 'react-router-dom'
+// import getData api call from actions
+
+class TrialsList extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default TrialsList;
