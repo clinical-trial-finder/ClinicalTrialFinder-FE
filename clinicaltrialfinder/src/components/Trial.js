f75dc9b07d65ed065378d3b7fe04bc75fd5c50a3
Zurglots <bfeole@gmail.com>
2019-06-27 16:48:50 -0400

    Trial text updated

--- clinicaltrialfinder/src/components/Trial.js
+++ clinicaltrialfinder/src/components/Trial.js
@@ -1,12 +1,18 @@
 import React from "react";
 // import PercentageCircle from "reactjs-percentage-circle";
+// import Collapsible from "react-collapsible";
 
 const Trial = props => {
   return (
     <div className="trial">
-      <p>Name: {props.trial.brief_title}</p>
-      <p>Trial type: {props.trial.study_type}</p>
-      <p>Phase: {props.trial.phase}</p>
+      <p>Name: </p>
+      <p>{props.trial.brief_title}</p>
+      <p>Trial type: </p>
+      <p>{props.trial.study_type}</p>
+      <p>Phase: </p>
+      <p>{props.trial.phase}</p>
+      <p>Description: </p>
+      <p>{props.trial.description}</p>
     </div>
   );
 };
